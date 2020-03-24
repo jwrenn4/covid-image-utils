c0969445d7229858a195f777e9f10d4b1249680c
Jacob <jwrenn4@outlook.com>
2020-03-24 07:23:10 -0400

    adding utilities to load the images, convert them to different file sizes, and save them

---
+++ loading_functions.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+import os
+
+load_image = lambda filepath : Image.open(filepath)
+
+def get_image_sizes(img_dir):
+    arrs = [
+        np.asarray(load_image(os.path.join(img_dir, img))) for img in os.listdir(img_dir)
+    ]
+    return np.unique([arr.shape[0] for arr in arrs]), np.unique([arr.shape[1] for arr in arrs])
+
+def convert_image_size(image, heights, widths):
+    average_size = (int(heights.mean()), int(widths.mean()))
+    converted_images = [
+        image.resize((h, w)).resize(average_size) for h in tqdm(heights) for w in widths
+    ]
+    return converted_images
+
+def convert_and_save(img_loc, save_dir, heights, widths):
+    base_fn = os.path.basename(img_loc)
+    image = load_image(img_loc)
+    converted_images = convert_image_size(image, heights, widths)
+    for i in range(len(converted_images)):
+        converted_images[i].save(os.path.join(save_dir, f'{i}_{base_fn}'))
